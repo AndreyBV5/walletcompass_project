99318e88b4d70b1b349a7b54f840770d5a0dbe82
AndreyBV5 <andrey0130player@gmail.com>
2024-05-17 01:50:47 -0600

    Update nuevo boton volver en create_identification

--- lib/modules_pages/create_identification.dart
+++ lib/modules_pages/create_identification.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Importa el paquete Firestore
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:copia_walletfirebase/model/identification_card.dart';
-import 'package:quickalert/quickalert.dart'; // Importa el paquete QuickAlert
+import 'package:quickalert/quickalert.dart';
 
 class CreateIdentificationForm extends StatefulWidget {
-  const CreateIdentificationForm({super.key});
+  const CreateIdentificationForm({Key? key}) : super(key: key);
 
   @override
   State<CreateIdentificationForm> createState() => _CreateIdentificationFormState();
@@ -16,7 +16,7 @@ class _CreateIdentificationFormState extends State<CreateIdentificationForm> {
   late final TextEditingController firstLastnameController;
   late final TextEditingController secondLastnameController;
 
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance; // Instancia de Firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
   void initState() {
@@ -38,18 +38,14 @@ class _CreateIdentificationFormState extends State<CreateIdentificationForm> {
 
   Future<void> _saveDataToFirestore() async {
     try {
-      // Guarda los datos en Firestore en la colección TarjetaCedula
       await _firestore.collection('TarjetaCedula').add({
         'nombreCompleto': holderNameController.text,
         'numeroCedula': idNumberController.text,
         'primerApellido': firstLastnameController.text,
         'segundoApellido': secondLastnameController.text,
       });
-
-      // Mostrar la alerta de éxito usando QuickAlert y redirigir después de 2 segundos
       _showSuccessAlert();
     } catch (e) {
-      // Manejo de errores
       print('Error al guardar los datos: $e');
     }
   }
@@ -64,7 +60,6 @@ class _CreateIdentificationFormState extends State<CreateIdentificationForm> {
       showConfirmBtn: false,
     );
 
-    // Esperar 2 segundos antes de navegar a la pantalla de inicio
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacementNamed('/home');
     });
@@ -73,23 +68,23 @@ class _CreateIdentificationFormState extends State<CreateIdentificationForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Creando identificación'),
-      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              IdentificationCard(
-                idNumber: idNumberController.text,
-                holderName: holderNameController.text,
-                firstLastname: firstLastnameController.text,
-                secondLastname: secondLastnameController.text,
-                idBackgroundImageAssetPath: "assets/images/card_bg.png",
-                logoAssetPath: "assets/images/bandera-costarica.png",
-                profileImageAssetPath: "assets/images/perfilcedula.jpeg",
+              Container(
+                margin: const EdgeInsets.only(top: 60), // Ajusta el margen inferior
+                child: IdentificationCard(
+                  idNumber: idNumberController.text,
+                  holderName: holderNameController.text,
+                  firstLastname: firstLastnameController.text,
+                  secondLastname: secondLastnameController.text,
+                  idBackgroundImageAssetPath: "assets/images/card_bg.png",
+                  logoAssetPath: "assets/images/bandera-costarica.png",
+                  profileImageAssetPath: "assets/images/perfilcedula.jpeg",
+                ),
               ),
               const SizedBox(height: 20),
               TextFormField(
@@ -113,20 +108,38 @@ class _CreateIdentificationFormState extends State<CreateIdentificationForm> {
                 onChanged: (_) => setState(() {}),
               ),
               const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: _saveDataToFirestore, // Llama al método para guardar en Firestore
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black, // Color de fondo negro
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8), // Bordes más cuadrados
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: _saveDataToFirestore,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      'Crear Identificación',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  child: const Text(
-                    'Crear Identificación',
-                    style: TextStyle(color: Colors.white), // Letras blancas
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacementNamed('/home');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      'Volver',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
+                ],
               ),
             ],
           ),
