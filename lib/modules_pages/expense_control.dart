c8ab09a87da2e62625b53e3421cc18769fa33589
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2024-05-16 20:24:19 -0600

    Update some components new

--- lib/modules_pages/expense_control.dart
+++ lib/modules_pages/expense_control.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseControl extends StatefulWidget {
@@ -10,22 +11,90 @@ class ExpenseControl extends StatefulWidget {
 }
 
 class _ExpenseControlState extends State<ExpenseControl> {
-  //Abrir nuevo expense box
-  void openNewExpenseBox() {
-    showDialog(
-        context: context,
-        builder: (contex) => AlertDialog(
-              title: Text("Nuevo gasto"),
-            ));
-  }
+  TextEditingController nameController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: openNewExpenseBox,
         child: const Icon(Icons.add),
       ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Gastos').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+          return ListView.builder(
+            itemCount: snapshot.data.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot expense = snapshot.data.docs[index];
+              return ListTile(
+                title: Text(expense['nombre']),
+                trailing: Text(expense['monto'].toString()),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void openNewExpenseBox() {
+    showDialog(
+      context: context,
+      builder: (contex) => AlertDialog(
+        title: const Text("Nuevo gasto"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(hintText: "Nombre"),
+            ),
+            TextField(
+              controller: amountController,
+              decoration: const InputDecoration(hintText: "Gasto"),
+            )
+          ],
+        ),
+        actions: [
+          _cancelButton(),
+          _createNewExpenseButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _cancelButton() {
+    return MaterialButton(
+      onPressed: () {
+        Navigator.pop(context);
+        nameController.clear();
+        amountController.clear();
+      },
+      child: const Text('Cancelar'),
+    );
+  }
+
+  Widget _createNewExpenseButton() {
+    return MaterialButton(
+      onPressed: () {
+        if (nameController.text.isNotEmpty &&
+            amountController.text.isNotEmpty) {
+          Navigator.pop(context);
+          FirebaseFirestore.instance.collection('Gastos').add({
+            'nombre': nameController.text,
+            'monto': double.parse(amountController.text),
+            'fecha': DateTime.now(),
+          });
+          nameController.clear();
+          amountController.clear();
+        }
+      },
+      child: const Text('Aceptar'),
     );
   }
 }
