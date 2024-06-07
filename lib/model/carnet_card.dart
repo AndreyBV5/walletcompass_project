64697df2a87d8e4a169ea86aa44579eafbc95cbd
AndreyBV5 <andrey0130player@gmail.com>
2024-06-07 10:22:28 -0600

    Update Carnet estudiantil

--- lib/model/carnet_card.dart
+++ lib/model/carnet_card.dart
@@ -20,44 +20,123 @@ class CarnetEstudiante extends StatelessWidget {
       height: 250,
       width: 350,
       decoration: BoxDecoration(
-        color: Colors.blue,
+        color: Colors.red,
         borderRadius: BorderRadius.circular(15),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.all(10),
+        child: Stack(
           children: [
-            Text(
-              numeroTarjeta,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 19,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 5),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 130),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Carné N°',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        numeroCarnet,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  nombreTitular,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  apellidosTitular,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  numeroTarjeta,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Vence 12/2026',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Placeholder(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Positioned(
+              top: 0,
+              right: 10,
+              child: Image.asset(
+                'assets/images/BN_logo.png',
+                width: 40,
+                height: 40,
               ),
             ),
-            Text(
-              nombreTitular,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                'assets/images/LOGO.png',
+                width: 70,
+                height: 70,
               ),
             ),
-            Text(
-              apellidosTitular,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              numeroCarnet,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+            Positioned(
+              top: 50,
+              right: 0,
+              child: Container(
+                width: 90,
+                height: 90,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(
+                      10),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/perfilcedula.jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
             ),
           ],
